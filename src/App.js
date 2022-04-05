d9d9b3573025dd334bbae567cb970e4165bce93f
tataqt <tata_98@ukr.net>
2022-04-05 14:02:23 +0300

    config chat in chatengine

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-function App() {
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
   return (
-    <div className="App">
-     test
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="5e565e0a-fc0f-4635-9d1e-26cdb8587571"
+      userName="tata"
+      userSecret="123123"
+    />
   );
 }
 
