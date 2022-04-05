59d29ed033ed91b902a481c1e80817abd47af138
tataqt <tata_98@ukr.net>
2022-04-05 14:50:51 +0300

    customize own chat feed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       projectID="5e565e0a-fc0f-4635-9d1e-26cdb8587571"
       userName="tata"
       userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/> }
     />
   );
 }
