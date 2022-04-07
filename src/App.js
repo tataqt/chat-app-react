a21fb30c97c17ecdfdb929935c8e8c7f51b885ab
tataqt <tata_98@ukr.net>
2022-04-07 11:45:29 +0300

    some fix

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-
 const App = () => {
-  if (!localStorage.getItem('username') && !localStorage.getItem('password')) return <LoginForm />
+  if (!localStorage.getItem('username') && !localStorage.getItem('password'))
+    return <LoginForm />
 
-  return (<
-    ChatEngine height="100vh"
+  return (<ChatEngine height="100vh"
     projectID="5e565e0a-fc0f-4635-9d1e-26cdb8587571"
     userName={localStorage.getItem('username')}
     userSecret={localStorage.getItem('password')}
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -50,7 +50,7 @@ const ChatFeed = (props) => {
             <div className="chat-title-container">
                 <div className="chat-title">{chat?.title}</div>
                 <div className="chat-subtitle">
-                    {chat.people.map((person) => ` ${person.person.username}`)}
+                    {chat.people.map((person) => `| ${person.person.username} |  `)}
                 </div>
             </div>
 
